b2c155c251560d839c186ebc38ec5e32f8b39384
BibhuPrasadBaliarsingh <bibhuprasadbaliarsingh@gmail.com>
2026-03-13 10:59:47 +0530

    update

--- server.js
+++ server.js
@@ -16,7 +16,13 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://carwash.onrender.com"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
